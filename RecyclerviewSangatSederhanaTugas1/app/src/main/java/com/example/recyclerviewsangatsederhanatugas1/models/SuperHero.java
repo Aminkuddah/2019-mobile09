5071162baf7e940764cff0620329f694c797f341
amin kuddah <aminkuddah@gmail.com>
2019-10-31 05:47:22 +0700

    Tugas 1

--- RecyclerviewSangatSederhanaTugas1/app/src/main/java/com/example/recyclerviewsangatsederhanatugas1/models/SuperHero.java
+++ RecyclerviewSangatSederhanaTugas1/app/src/main/java/com/example/recyclerviewsangatsederhanatugas1/models/SuperHero.java
@@ -1,30 +1,70 @@
 package com.example.recyclerviewsangatsederhanatugas1.models;
 
 public class SuperHero {
-    public int gambarHeroName;
-    public String deskripsi;
+//    public int gambarHero;
+//    public String deskripsi;
+//
+//    public SuperHero(int heroName) {
+//
+//        this.gambarHero= gambarHero;
+//    }
+//
+//    public int getGambarHero() {
+//
+//        return gambarHero;
+//    }
+//
+//    public void setGambarHero(String heroName) {
+//
+//        this.gambarHero = gambarHero;
+//    }
+//
+//    public SuperHero(int joker10, String deskripsi) {
+//        this.deskripsi = deskripsi;
+//    }
+//
+//    public String getDeskripsi() {
+//        return deskripsi;
+//    }
+//
+//    public void setDeskripsi(String heroName) {
+//
+//        this.deskripsi = deskripsi;
+//    }
 
-    public SuperHero(int heroName) {
-        this.gambarHeroName = gambarHeroName;
+    public String heroName;
+    public String heroText;
+    public int heroImage;
+
+    public SuperHero(String heroName, String heroText, int heroImage) {
+        this.heroName = heroName;
+        this.heroText = heroText;
+        this.heroImage = heroImage;
     }
 
-    public int getGambarHeroName() {
-        return gambarHeroName;
+    public String getHeroName() {
+        return heroName;
     }
 
-    public void setGambarHeroName(String heroName) {
-        this.gambarHeroName = gambarHeroName;
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
     }
 
-    public SuperHero(String deskripsi) {
-        this.deskripsi = deskripsi;
+    public String getHeroText() {
+        return heroText;
     }
 
-    public String geteskripsi() {
-        return deskripsi;
+    public void setHeroText(String heroText) {
+        this.heroText = heroText;
     }
 
-    public void setDeskripsi(String heroName) {
-        this.deskripsi = deskripsi;
+
+    public int getHeroImage() {
+        return heroImage;
+    }
+
+
+    public void setHeroImage(int heroImage) {
+        this.heroImage = heroImage;
     }
 }
